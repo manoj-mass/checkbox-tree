364cf9ada644d696af2014411fff2a2e65c70b0b
Manoj Masakorala <manoj.masakorala@gmail.com>
2021-07-05 21:40:09 +0530

    project sturcutre fix

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import setupAxiosInterceptors from './config/axios-interceptor';
-import ErrorBoundary from './shared/error/error-boundary';
 import AppComponent from './app';
 
 const rootEl = document.getElementById('root');
@@ -10,11 +8,9 @@ const rootEl = document.getElementById('root');
 const render = Component =>
   // eslint-disable-next-line react/no-render-return-value
   ReactDOM.render(
-    <ErrorBoundary>
-      <div>
-        <Component />
-      </div>
-    </ErrorBoundary>,
+    <div>
+      <Component />
+    </div>,
     rootEl
   );
 
